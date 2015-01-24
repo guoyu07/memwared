9b20ddfe505bcab22ac2154d28927ba882204b42
rryqszq4 <279664262@163.com>
2015-01-24 18:20:52 +0800

    [memwared] coremax and root
    limit coremax file if
    add username

--- memwared.c
+++ memwared.c
@@ -12,6 +12,10 @@
 #include <sysexits.h>
 #include <signal.h>
 #include <unistd.h>
+#include <limits.h>
+#include <sys/resource.h>
+#include <pwd.h>
+
 
 static void 
 sig_handler(const int sig)
@@ -43,6 +47,11 @@ main (int argc, char **argv)
 {
 	int c;
 	enum hashfunc_type hash_type = JENKINS_HASH;
+	int maxcore = 0;
+	struct rlimit rlim;
+	char *username = NULL;
+	struct passwd *pw;
+
 
 	if (!sanitycheck()){
 		return EX_OSERR;
@@ -55,12 +64,20 @@ main (int argc, char **argv)
 	//setbuf(stderr, NULL);
 	
 	while (-1 != (c = getopt(argc, argv,
-			"p:"
+			"p:" // TCP port
 			"P:"
+			"r:" // maxcore core file limit
+			"u:" // user identity to run as
 		))){
 		switch (c){
 		case 'p':
 			printf("TCP port: %d\n",atoi(optarg));
+			break;
+		case 'r':
+			maxcore = 1;
+			break;
+		case 'u':
+			username = optarg;
 			break;
 		default:
 			fprintf(stderr, "Illegal argument \"%c\"\n",c );
@@ -71,6 +88,50 @@ main (int argc, char **argv)
 	if (hash_init(hash_type) != 0){
 		fprintf(stderr, "Failed to initialize hash_algorithm!\n");
 		exit(EX_USAGE);
+	}
+
+	/* maxcore file limit */
+	if (maxcore != 0){
+		struct rlimit rlim_new;
+		if (getrlimit(RLIMIT_CORE, &rlim) == 0){
+			rlim_new.rlim_cur = rlim_new.rlim_max = RLIM_INFINITY;
+			if (setrlimit(RLIMIT_CORE, &rlim_new) != 0){
+				rlim_new.rlim_cur = rlim_new.rlim_max = rlim.rlim_max;
+				(void)setrlimit(RLIMIT_CORE, &rlim_new);
+			}
+		}
+
+		if ((getrlimit(RLIMIT_CORE, &rlim) != 0) || rlim.rlim_cur == 0){
+			fprintf(stderr, "failed to ensure corefile creation\n");
+			exit(EX_OSERR);
+		}
+	}
+	
+	if (getrlimit(RLIMIT_NOFILE, &rlim) != 0){
+		fprintf(stderr, "failed to getrlimit number of files\n");
+		exit(EX_OSERR);
+	}else {
+		rlim.rlim_cur = 1024;
+		rlim.rlim_max = 1024;
+		if (setrlimit(RLIMIT_NOFILE, &rlim) != 0){
+			fprintf(stderr, "failed to set rlimit for open files. Try starting as root or requesting smaller maxconns value.\n");
+			exit(EX_OSERR);
+		}
+	}
+
+	if (getuid() == 0 || geteuid() == 0){
+		if (username == 0 || *username == '\0'){
+			fprintf(stderr, "can't run as root without the -u switch\n");
+			exit(EX_USAGE);
+		}
+		if ((pw = getpwnam(username)) == 0){
+			fprintf(stderr, "can't find the user %s to switch to \n", username);
+			exit(EX_NOUSER);
+		}
+		if (setgid(pw->pw_gid) < 0 || setuid(pw->pw_uid) < 0){
+			fprintf(stderr, "failed to assume identity of user %s \n", username);
+			exit(EX_OSERR);
+		}
 	}
 
 	return 0;
